69f63809649b1a65157350e3fd8211c5b3eab7b3
aleksandr.kalyuganov <aleksandr.kalyuganov@gmail.com>
2024-12-01 16:06:50 +0300

    Add test project

---
+++ GitHubActionsDotNet.Api.Tests/Models/WeatherForecastTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using GitHubActionsDotNet.Api.Models;
+
+namespace GitHubActionsDotNet.Api.Tests.Models
+{
+    public class WeatherForecastTests
+    {
+        [Fact]
+        public void TemperatureFShouldReturnCorrectValueBasedOnTemperatureC()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                TemperatureC = 0
+            };
+
+            weatherForecast.TemperatureF.Should().Be(32);
+        }
+    }
+}
